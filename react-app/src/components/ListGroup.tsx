a1955eb7cf9a6b64e0d45927841ad2eda01bb3b5
Sanjay Tatineni <statineni2@gmail.com>
2025-08-18 11:33:09 -0500

    all changes

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,16 @@
 function ListGroup() {
+  let items = ["Houston", "Dallas", "Austin", "San Antonio", "El Paso"];
+  //   items = [];
   return (
-    <ul className="list-group">
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      {items.length === 4 && <p>No items found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item">{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
